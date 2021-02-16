7afdfeb730cfae1de60ee0b3fb644a5b2bc9b45c
Chien Yu Kuo <josh820217@gmail.com>
2021-02-16 21:30:55 +0800

    practing uploading procedure

---
+++ hi_adding_procedure.py
@@ -0,0 +1,13 @@
+name = input('請輸入名字：')
+print('嗨', name)
+Weight = input('請輸入體重(kg): ')
+Height = input('請輸入身高(cm): ')
+print('嗨', name, '您的體重與身高為', Weight, 'kg', Height, 'cm')
+#These are basic information for GitHub practice
+#echo "# Angular-First-Project"
+#git init
+#git add README.md
+#git commit -m "版本訊息"
+#git branch -M main
+#git remote add origin 'link'
+#git push -u origin main
